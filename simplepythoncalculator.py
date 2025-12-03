fe5c723c82c8a918c98dcd27ae35156fd036dc38
Samuel Lire <samuellire076@gmail.com>
2025-12-03 14:48:37 +0300

    Add basic arithmetic functions to calculator
    Implement basic arithmetic operations in a simple calculator.

---
+++ simplepythoncalculator.py
@@ -0,0 +1,30 @@
+print("insert first number")
+global x
+x=int(input("first number:"))
+global y 
+print("insert first number")
+y=int("second number:)
+
+
+      
+def add(num1,num2):
+  return num1+num2 
+
+def multply(num1,num2):
+  return num1*num2 
+
+def divide(num1,num2):
+  return num1/num2
+  
+def subtract(num1,num2):
+  return num1-num2 
+
+def modulo(num1,num2):
+  return num1%num2 
+
+
+
+  
+
+
+
