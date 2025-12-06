fd67352effb427832824a0cd323673bdffa064eb
Samuel Lire <samuellire076@gmail.com>
2025-12-06 10:35:43 +0300

    Create evencheck.py

---
+++ evencheck.py
@@ -0,0 +1,8 @@
+def evenOddCheck(number):
+    if number%2==0:
+        print(number," is even")
+    else:
+        print(number," is odd")
+        
+number=int(input("Enter number you want to check:"))
+evenOddCheck(number)
