80f6c7bdfa511fbd1ab1c06ea27b2a695ebf1c8a
Samuel Lire <samuellire076@gmail.com>
2025-12-06 10:23:30 +0300

    Refactor calculator to use operator input for operations

--- simplepythoncalculator.py
+++ simplepythoncalculator.py
@@ -16,10 +16,17 @@ def subtract(number1,number2):
 print("Enter two numbers")
 x=int(input("First Number: "))
 y=int(input("Second Number: "))
-print("The addition result is ",add(x,y))
-print("The multplication result is ",multiply(x,y))
-print("The division result is ",divide(x,y))
-print("The subtraction result is ",subtract(x,y))
+operator=input("Enter opreator for your operation you want to perform in the above two numbers")
+if operator=="+":
+    print("The addition result is ",add(x,y))
+elif operator=="*":
+    print("The multplication result is ",multiply(x,y))
+elif operator=="/":
+    print("The division result is ",divide(x,y))
+elif operator=="-":
+    print("The subtraction result is ",subtract(x,y))
+else:
+    print("Invalid operator")
       
       
       
