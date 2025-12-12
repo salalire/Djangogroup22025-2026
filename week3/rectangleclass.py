f580288a59d477a0226fb5627dede01ca3e641c8
Samuel Lire <samuellire076@gmail.com>
2025-12-12 20:21:40 +0300

    Create rectangleclass.py

---
+++ week3/rectangleclass.py
@@ -0,0 +1,7 @@
+class rectangle:
+    def __init__(self,width,height):
+        self.width=width
+        self.height=height
+        print("The area of the rectangle is",self.width*self.height)
+
+rect=rectangle(2,5)
