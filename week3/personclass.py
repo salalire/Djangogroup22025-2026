b74bb5d2beb5daf7726fe69f8910b296d70ccde6
Samuel Lire <samuellire076@gmail.com>
2025-12-12 19:47:53 +0300

    Create personclass.py

---
+++ week3/personclass.py
@@ -0,0 +1,12 @@
+class person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def printaributes(self):
+        print("Name: ",self.name)
+        print("Age: ",self.age)
+
+
+person1=person("samuel",22)
+person1.printaributes()
+
