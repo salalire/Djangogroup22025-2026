b072d1129b853aea1258701cd5c7ba38c7bcb33b
Salah <samuellire076@gmail.com>
2026-02-18 12:20:07 +0300

    add update and delete

--- project/manage_book/book_managment/urls.py
+++ project/manage_book/book_managment/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns=[
     path("list/",views.display_books), 
     path("<int:book_id>/",views.get_one_book), 
+    path("<int:book_id>/update/",views.update_book), 
     path("create/",views.create_book),
+    path("<int:book_id>/delete/",views.delete_book), 
 ]
--- project/manage_book/book_managment/views.py
+++ project/manage_book/book_managment/views.py
@@ -66,3 +66,57 @@ def create_book(request):
         },
         status=404
         )
+        
+@csrf_exempt       
+def update_book(request,book_id):
+    if request.method!="PUT":
+        return JsonResponse({
+            "Error":"method not matched"
+        },
+        status=405
+        )
+    try:
+        data=json.loads(request.body)
+        book=Book.objects.get(id=book_id)
+        book.title=data["title"]
+        book.author=Author.objects.get(id=data["author_id"])
+        book.published_date=data["published_date"]
+        book.isbn=data["isbn"]
+        book.save()
+        return JsonResponse({
+            "id":book_id,
+            "message":"book updated"
+        },
+        status=201
+        )
+    except Author.DoesNotExist:
+        return JsonResponse({
+            "Error":"author not found"
+        },
+        status=404
+        )
+@csrf_exempt       
+def delete_book(request,book_id):
+    if request.method!="DELETE":
+        return JsonResponse(
+            {
+                "message":"method is not allowed "
+            },
+            status=405
+        )
+    try:
+        book=Book.objects.get(id=book_id)
+        book.delete()
+        return JsonResponse(
+            {
+                "message":"book deleted successfully"
+            },
+            status=203
+        )
+    except Book.DoesNotExist:
+        return JsonResponse(
+            {
+                "Error":"Book does not exist"  
+            },
+            status=404
+        )
